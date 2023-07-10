5d96c1658c29994bb64b7a8234ad99d6320e9d92
Lucas Silva <lucascattosilva2018@gmail.com>
2023-07-10 17:49:14 -0300

    Criando site com BootStrap 1/5

--- index.php
+++ index.php
@@ -6,13 +6,12 @@
         <title>Projeto Dev Web</title>
         <!-- BootStrap Import -->
         <link rel="stylesheet" href="./css/bootstrap.min.css">
+        <!-- Own CSS -->
+        <link rel="stylesheet" href="./css/style.css">
     </head>
     <body>
-    
-
-
-
-        <nav class="navbar navbar-default navbar-fixed-top">
+        <!-- ==================================================================================================== -->
+        <nav class="navbar navbar-default navbar-fixed-top"> <!-- navbar-fixed-top -->
             <div class="container">
                 <div class="navbar-header">
                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -21,8 +20,8 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="#">DevWeb</a>
-                </div>
+                    <a class="navbar-brand" href="#">Danki Code</a>
+                </div> <!-- navbar-header -->
                 <div id="navbar" class="collapse navbar-collapse">
                     <ul class="nav navbar-nav navbar-right">
                         <li class="active"><a href="#">Home</a></li>
@@ -30,9 +29,25 @@
                         <li><a href="#contact">Contact</a></li>
                     </ul>
                 </div>
-            </div>
+            </div> <!-- container -->
         </nav>
-    
+        <!-- ==================================================================================================== -->
+        <div class="box">
+            <section class="banner">
+                <div class="overlay">
+
+                </div>
+                <div class="container chamada-banner">
+                    <div class="row">
+                        <div class="col-md-12 text-center">
+                            <h2><?php echo htmlentities('<');?>Danki.Code<?php echo htmlentities('>');?></h2>
+                            <p>Empresa voltada para desenvolvimento web e marketing digital</p>
+                            <a href="">Saiba Mais!</a>
+                        </div> <!-- col-md-12 -->
+                    </div> <!-- row -->
+                </div> <!-- container -->
+            </section>
+        </div> <!-- box -->
 
 
 
